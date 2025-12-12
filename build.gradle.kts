ba78c87ba220378f6ffe81b2d548f1ecbfaef049
Magnus Johansson <kkckkc@gmail.com>
2025-12-12 10:34:09 +0100

    Remove description extraction

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ intellijPlatform {
         version = providers.gradleProperty("pluginVersion")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+        /*description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
             val start = "<!-- Plugin description -->"
             val end = "<!-- Plugin description end -->"
 
@@ -61,7 +61,7 @@ intellijPlatform {
                 }
                 subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
             }
-        }
+        }*/
 /*
         val changelog = project.changelog // local variable for configuration cache compatibility
         // Get the latest available change notes from the changelog file
